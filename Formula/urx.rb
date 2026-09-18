622b071c491f66a521e309b3ff10f81164bf0a16
hahwul <hahwul@gmail.com>
2026-09-18 15:01:34 +0000

    chore: brew formula update for urx 0.11.0

--- Formula/urx.rb
+++ Formula/urx.rb
@@ -5,8 +5,8 @@
 class Urx < Formula
   desc "Extracts urls from osint archives for security insights"
   homepage "https://github.com/hahwul/urx"
-  url "https://github.com/hahwul/urx/archive/refs/tags/0.10.0.tar.gz"
-  sha256 "d05f11d2cb2994c3d79b15a563685cc2b8ca5d9acf4af8ab659ae1d280d58931"
+  url "https://github.com/hahwul/urx/archive/refs/tags/0.11.0.tar.gz"
+  sha256 "ff8f5d9bbd4c3ca1c3cdafb17a4742bc0f8a132ca2edc9b76d9c871af3cb4dec"
   license "MIT"
 
   depends_on "rust"
